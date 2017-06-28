72437f4e54c53d4a2973f4e52493056299b3ec31
Daniel Hernandez <daniel@Daniels-MacBook-Pro.local>
2017-06-27 20:56:07 -0500

    dyanmic attack button displayed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,16 @@
+var player1 = {
+	
+}
+
+
+
+
+
 $('.player').hover(
 	function() {
-		$(this).find('h3').show();
+		$(this).find('.destroy').show();
 	}, function() {
-		$(this).find('h3').hide();
+		$(this).find('.destroy').hide();
 	}
 );
 //$('.player').one('click', function() {
@@ -11,10 +19,15 @@ $('.player').hover(
 //Those event handlers are then in place FOREVER. They no longer look at what classes any elements have. 
 //This means that changing a class after you install a static event handler does not affect which elements have event handlers on them BECAUSE THEY HAVE ALREADY BEEN ADDED.
 $(document).one('click', '.player', function() {
-	console.log('You clicked');
-	$(this).siblings().addClass('enemies');
+	console.log('You clicked a player');
+	$(this).siblings().addClass('enemy');
 	$('.player').toggleClass('m-0a');	
 	$(this).siblings().appendTo('#enemyBoard');
 	$('#enemies').show();
-	$('#enemies').removeClass('player');
+	$('.enemy').click(function() {
+		console.log("you clicked a enemy");
+		$('#attack').show();
+	});
 });
+
+
