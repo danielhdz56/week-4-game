7c048efd07652f409538d2cd99f82561b31d06a7
Daniel Hernandez <daniel@Daniels-MacBook-Pro.local>
2017-06-29 16:04:45 -0500

    Modified code into functions for reuse when opponent is defeated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,72 +22,97 @@ var characters = [ {
 }, {
 	id: 2,
 	name: "Anakin Skywalker",
-	img: "../assets/images/dvPlayer.svg",
+	img: "assets/images/dvPlayer.svg",
 	life: 100,
 	attack: 10,
 	computerAttack: 25
 }, {
 	id: 3,
 	name: "R2D2",
-	img: "..assets/images/rPlayer.svg",
+	img: "assets/images/rPlayer.svg",
 	life: 100,
 	attack: 10,
 	computerAttack: 25
 }];
-//Creating the characthers
-
-
-
-
-
-$('.characters').hover(
-	function() {
-		$(this).find('.destroy').show();
-	}, function() {
-		$(this).find('.destroy').hide();
+//Creating the characters
+function startGame() {
+	for(i = 0; i < characters.length; i++) {
+		var charactherChoices = $('<div>');
+		var box = $('<div>');
+		var img = $('<img>');
+		var overlay = $('<div>');
+		var name = $('<h3>');
+		var life = $('<p>');
+		charactherChoices.attr('id', i);
+		charactherChoices.addClass('col-3 characters');
+		charactherChoices.appendTo('#characters');
+		box.addClass('box');
+		box.appendTo(charactherChoices);
+		img.addClass('img-fluid');
+		//can you put more attributes in one line?
+		img.attr('src', characters[i].img);
+		img.attr('alt', characters[i].name);
+		img.appendTo(box);
+		overlay.addClass('box-img-overlay');
+		overlay.hide();
+		overlay.appendTo(box);
+		name.addClass('box-title');
+		name.text(characters[i].name);
+		name.appendTo(overlay);
+		life.text('Life: ' + characters[i].life);
+		life.appendTo(overlay)	
 	}
-);
+}
+startGame();
+//Hover, remember that the hovering is attached to the element, not the character class
+//This means that when I remove the character class from them they will still have the hovering capability
+function hovering() {
+	$('.characters').hover(
+		function() {
+			$(this).find('.box-img-overlay').show();
+		}, function() {
+			$(this).find('.box-img-overlay').hide();
+		}
+	);
+}
+hovering();
+//CharacterSettingClick
 function characterSettingClick() {
-	$(document).on('click', '.characters', function(){
+	$(document).on('click', '.characters', function() {
+		//Will only run if I haven't picked a player
 		if(!pickedPlayer) {
-			myPlayer = $(this).attr('id');
-			$(this).siblings().addClass('enemy');
-			$('.characters').toggleClass('m-0a');	
-			$(this).siblings().appendTo('#enemyBoard');
-			$('#enemies').show();
+			$('#enemyBoard').show();
 			$(this).removeClass('characters');
 			pickedPlayer = true;
-			console.log("myPlayer has the following id: " + myPlayer);
-			myPlayer = characters[myPlayer];
-			console.log(myPlayer);
-		}
-		else if(pickedPlayer && !pickedOpponent) {
+			$(this).siblings().appendTo($('#enemies'));
+			//Got the id attribute of whatever characther i clicked and passed it to myPlayer
+			myPlayer = $(this).attr('id');
+			//I then targeted the heading, chooseTextCharacter, and replaced by the name of the characther I chose
+			$('#chooseTextCharacter').text(characters[myPlayer].name);
+		//Will only run if I have picked a player and if I haven't picked an Opponent
+		} else if(pickedPlayer && !pickedOpponent) {
+			$('#benchedBoard').show();
+			$('#attack').show();
 			myOpponent = $(this).attr('id');
-			$(this).siblings().addClass('benched');
-		  	$(this).siblings().appendTo('#benchedBoard');
-		 	$('#attack').show();
-		 	$('#bench').show();
-			pickedOpponent = true;
-			console.log("myOpponent has the following id: " + myOpponent);
-			myOpponent = characters[myOpponent];
-			console.log(myOpponent.life);
+			$('#chooseTextOpponent').text(characters[myOpponent].name)
+			$(this).nextAll().addBack().removeClass('characters');
+			$(this).siblings().appendTo($('#benched'));
 		}
-	});
+	})
 }
 characterSettingClick();
-
-function combatAttackClick() {
-	$('#attack').click(function() {
-		//Player attacking opponent it is important that the variable i is defined at the Global Scope or else it will always reset on click
-		myOpponent.life = myOpponent.life - (myPlayer.attack * i);
-		i++;
-		$('#' + myOpponent.id).find("p").html("Life: " + myOpponent.life);
-		if( myOpponent.life <= 0) {
-			$('#' + myOpponent.id).remove();
-		}
-		//Opponent attacking player
-		myPlayer.life = myPlayer.life - myOpponent.computerAttack;
-		$('#' + myPlayer.id).find("p").html("Life: " + myPlayer.life);
-	});
-}
-combatAttackClick();
+// function combatAttackClick() {
+// 	$('#attack').click(function() {
+// 		//Player attacking opponent it is important that the variable i is defined at the Global Scope or else it will always reset on click
+// 		myOpponent.life = myOpponent.life - (myPlayer.attack * i);
+// 		i++;
+// 		$('#' + myOpponent.id).find("p").html("Life: " + myOpponent.life);
+// 		if( myOpponent.life <= 0) {
+// 			$('#' + myOpponent.id).remove();
+// 		}
+// 		//Opponent attacking player
+// 		myPlayer.life = myPlayer.life - myOpponent.computerAttack;
+// 		$('#' + myPlayer.id).find("p").html("Life: " + myPlayer.life);
+// 	});
+// }
+// combatAttackClick();
