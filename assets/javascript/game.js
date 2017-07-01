d64e83c805cbfc6249ccda7249a0e52398c5ea5b
Daniel Hernandez <daniel56hdz@gmail.com>
2017-06-30 20:34:44 -0500

    Made game so only one route exist to win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,56 +10,56 @@ var characters = [ {
 	name: "Obi-Wan Kenobi",
 	img: "assets/images/bkPlayer.svg",
 	life: 100,
-	attack: 10,
-	computerAttack: 25
+	attack: 15,
+	computerAttack: 7
 }, {
 	id: 1,
 	name: "Boba Fett",
 	img: "assets/images/bPlayer.svg",
-	life: 100,
-	attack: 10,
-	computerAttack: 25
+	life: 120,
+	attack: 8,
+	computerAttack: 15
 }, {
 	id: 2,
 	name: "Anakin Skywalker",
 	img: "assets/images/dvPlayer.svg",
-	life: 100,
-	attack: 10,
-	computerAttack: 25
+	life: 165,
+	attack: 4,
+	computerAttack: 20
 }, {
 	id: 3,
 	name: "R2D2",
 	img: "assets/images/rPlayer.svg",
-	life: 100,
-	attack: 10,
+	life: 180,
+	attack: 3,
 	computerAttack: 25
 }];
 //Creating the characters
 function startGame() {
-	for(i = 0; i < characters.length; i++) {
+	for(j = 0; j < characters.length; j++) {
 		var charactherChoices = $('<div>');
 		var box = $('<div>');
 		var img = $('<img>');
 		var overlay = $('<div>');
 		var name = $('<h3>');
 		var life = $('<p>');
-		charactherChoices.attr('id', i);
+		charactherChoices.attr('id', j);
 		charactherChoices.addClass('col-3 characters');
 		charactherChoices.appendTo('#characters');
 		box.addClass('box');
 		box.appendTo(charactherChoices);
 		img.addClass('img-fluid');
 		//can you put more attributes in one line?
-		img.attr('src', characters[i].img);
-		img.attr('alt', characters[i].name);
+		img.attr('src', characters[j].img);
+		img.attr('alt', characters[j].name);
 		img.appendTo(box);
 		overlay.addClass('box-img-overlay');
 		overlay.hide();
 		overlay.appendTo(box);
 		name.addClass('box-title');
-		name.text(characters[i].name);
+		name.text(characters[j].name);
 		name.appendTo(overlay);
-		life.text('Life: ' + characters[i].life);
+		life.text('Life: ' + characters[j].life);
 		life.appendTo(overlay)	
 	}
 }
@@ -106,20 +106,48 @@ characterSettingClick();
 function combatAttackClick() {
 	$('#attack').click(function() {
 		//Player attacking opponent it is important that the variable i is defined at the Global Scope or else it will always reset on click
-		characters[myOpponent].life = characters[myOpponent].life - (characters[myPlayer].attack);
+		characters[myOpponent].life = characters[myOpponent].life - i * characters[myPlayer].attack;
+		// if (characters[myOpponent].life <= 0 && )
 		i++;
 		$('#' + myOpponent).find('p').text('Life: ' + characters[myOpponent].life)
 		if(characters[myOpponent].life <= 0) {
+			if($('.characters').length === 1) {
+				console.log("hello World!");
+				$('#attack').hide();
+				$('#chooseTextOpponent').hide();
+				$('#chooseTextCharacter').text(characters[myPlayer].name + " Wins");
+			}
 			$('#' + myOpponent).remove();
-			$('#chooseTextOpponent').text("Choose Another Opponent");
 			$('.benchedEnemies').addClass('characters');
-			$('.benchedEnemies').appendTo($('#enemies'));
 			$('#benchedBoard').hide();
-			$('#attack').hide();
-		}
-		//Opponent attacking player
-		myPlayer.life = myPlayer.life - myOpponent.computerAttack;
-		$('#' + myPlayer.id).find("p").html("Life: " + myPlayer.life);
+			if($('div.benchedEnemies').length === 1) {
+				myOpponent = $('.benchedEnemies').attr('id');
+				$('#chooseTextOpponent').text(characters[myOpponent].name);
+				$('.characters').removeClass('benchedEnemies');
+				//This makes it so the click doesn't work for the last enemy
+				pickedOpponent = true;
+				console.log("Hello1");
+			}
+			else if($('.characters').length !== 0) {
+				$('#chooseTextOpponent').text("Choose Another Opponent");
+				$('.characters').removeClass('benchedEnemies');
+				$('#attack').hide();
+				console.log("Hello2")
+			}
+			$('.characters').appendTo($('#enemies'));
+		} else {
+			//Opponent attacking player
+			characters[myPlayer].life = characters[myPlayer].life - characters[myOpponent].computerAttack;
+			myPlayer.life = myPlayer.life - myOpponent.computerAttack;
+			$('#' + myPlayer).find('p').text('Life: ' + characters[myPlayer].life);
+			if(characters[myPlayer].life <= 0) {
+				console.log("I have lost!");
+				$('#attack').hide();
+				$('#characterBoard').hide();
+				$('#benchedBoard').hide();
+				$('#chooseTextOpponent').text(characters[myOpponent].name + " DEFEATED YOU!");
+			}
+		}	
 	});
 }
 combatAttackClick();
