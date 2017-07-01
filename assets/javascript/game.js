4bca480daa36bdc8c0c3db5ba4d8ff4e3646dff9
Daniel Hernandez <daniel56hdz@gmail.com>
2017-07-01 00:33:55 -0500

    Turned life property to hp and got rid of hover

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,28 +9,28 @@ var characters = [ {
 	id: 0,
 	name: "Obi-Wan Kenobi",
 	img: "assets/images/bkPlayer.svg",
-	life: 100,
+	hp: 100,
 	attack: 15,
 	computerAttack: 7
 }, {
 	id: 1,
 	name: "Boba Fett",
 	img: "assets/images/bPlayer.svg",
-	life: 120,
+	hp: 120,
 	attack: 8,
 	computerAttack: 15
 }, {
 	id: 2,
 	name: "Anakin Skywalker",
 	img: "assets/images/dvPlayer.svg",
-	life: 165,
+	hp: 165,
 	attack: 4,
 	computerAttack: 20
 }, {
 	id: 3,
 	name: "R2D2",
 	img: "assets/images/rPlayer.svg",
-	life: 180,
+	hp: 180,
 	attack: 3,
 	computerAttack: 25
 }];
@@ -42,7 +42,7 @@ function startGame() {
 		var img = $('<img>');
 		var overlay = $('<div>');
 		var name = $('<h3>');
-		var life = $('<p>');
+		var hp = $('<h3>');
 		charactherChoices.attr('id', j);
 		charactherChoices.addClass('col-3 characters');
 		charactherChoices.appendTo('#characters');
@@ -59,14 +59,14 @@ function startGame() {
 		name.addClass('box-title');
 		name.text(characters[j].name);
 		name.appendTo(overlay);
-		life.text('Life: ' + characters[j].life);
-		life.appendTo(overlay)	
+		hp.text('HP: ' + characters[j].hp);
+		hp.appendTo(overlay)	
 	}
 }
 startGame();
 //Hover, remember that the hovering is attached to the element, not the character class
 //This means that when I remove the character class from them they will still have the hovering capability
-function hovering() {
+
 	$('.characters').hover(
 		function() {
 			$(this).find('.box-img-overlay').show();
@@ -74,13 +74,14 @@ function hovering() {
 			$(this).find('.box-img-overlay').hide();
 		}
 	);
-}
-hovering();
+
 //CharacterSettingClick
 function characterSettingClick() {
 	$(document).on('click', '.characters', function() {
-		//Will only run if I haven't picked a player
+	$(this).unbind("mouseenter");		
+	//Will only run if I haven't picked a player
 		if(!pickedPlayer) {
+			$(this).off('hovering');
 			$('#enemyBoard').show();
 			$(this).removeClass('characters');
 			pickedPlayer = true;
@@ -89,12 +90,14 @@ function characterSettingClick() {
 			myPlayer = $(this).attr('id');
 			//I then targeted the heading, chooseTextCharacter, and replaced by the name of the characther I chose
 			$('#chooseTextCharacter').text(characters[myPlayer].name);
+			$('#hpTextCharacter').text("HP: " + characters[myPlayer].hp);
 		//Will only run if I have picked a player and if I haven't picked an Opponent
 		} else if(pickedPlayer && !pickedOpponent) {
 			$('#benchedBoard').show();
 			$('#attack').show();
 			myOpponent = $(this).attr('id');
-			$('#chooseTextOpponent').text(characters[myOpponent].name)
+			$('#chooseTextOpponent').text(characters[myOpponent].name);
+			$('#hpTextOpponent').text("HP: " + characters[myOpponent].hp);
 			$(this).siblings().addClass('benchedEnemies');
 			$(this).siblings().addBack().removeClass('characters');
 			$(this).siblings().appendTo($('#benched'));
@@ -106,17 +109,17 @@ characterSettingClick();
 function combatAttackClick() {
 	$('#attack').click(function() {
 		//Player attacking opponent it is important that the variable i is defined at the Global Scope or else it will always reset on click
-		characters[myOpponent].life = characters[myOpponent].life - i * characters[myPlayer].attack;
-		// if (characters[myOpponent].life <= 0 && )
+		characters[myOpponent].hp = characters[myOpponent].hp - i * characters[myPlayer].attack;
+		$('#hpTextOpponent').text("HP: " + characters[myOpponent].hp);
+		// if (characters[myOpponent].hp <= 0 && )
 		i++;
-		$('#' + myOpponent).find('p').text('Life: ' + characters[myOpponent].life)
-		if(characters[myOpponent].life <= 0) {
+		if(characters[myOpponent].hp <= 0) {
 			if($('.characters').length === 1) {
-				console.log("hello World!");
 				$('#attack').hide();
 				$('#chooseTextOpponent').hide();
 				$('#chooseTextCharacter').text(characters[myPlayer].name + " Wins");
 			}
+			$('#hpTextOpponent').text("");
 			$('#' + myOpponent).remove();
 			$('.benchedEnemies').addClass('characters');
 			$('#benchedBoard').hide();
@@ -132,16 +135,14 @@ function combatAttackClick() {
 				$('#chooseTextOpponent').text("Choose Another Opponent");
 				$('.characters').removeClass('benchedEnemies');
 				$('#attack').hide();
-				console.log("Hello2")
 			}
 			$('.characters').appendTo($('#enemies'));
 		} else {
 			//Opponent attacking player
-			characters[myPlayer].life = characters[myPlayer].life - characters[myOpponent].computerAttack;
-			myPlayer.life = myPlayer.life - myOpponent.computerAttack;
-			$('#' + myPlayer).find('p').text('Life: ' + characters[myPlayer].life);
-			if(characters[myPlayer].life <= 0) {
-				console.log("I have lost!");
+			characters[myPlayer].hp = characters[myPlayer].hp - characters[myOpponent].computerAttack;
+			$('#hpTextCharacter').text("HP: " + characters[myPlayer].hp);
+			if(characters[myPlayer].hp <= 0) {
+				$('#lifeTextCharacter').text("");
 				$('#attack').hide();
 				$('#characterBoard').hide();
 				$('#benchedBoard').hide();
